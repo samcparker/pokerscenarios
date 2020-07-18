b71b4f360eedb98ab900dcfae378a8e84523c0db
Sam <samcparker1999@gmail.com>
2020-07-18 08:25:47 +0100

    e

---
+++ components/PokerTable.vue
@@ -0,0 +1,146 @@
+<template>
+<div>
+    <v-row justify="center" style="margin-bottom: 100px"><h1>Your Turn</h1></v-row>
+  <v-container style="max-width: 1200px; height: 500px; border-radius: 5%; margin-bottom: 50px"  class="table" fill-height>
+
+    <v-row style="transform: translateY(-100px)">
+          <v-col cols="4">
+            <v-row justify="center">
+              <v-col v-if="showNames" cols="12">
+                <p class="text-center">COMPUTER</p>
+              </v-col>
+              <v-col class="card">
+                <v-img src="/card-back.png"></v-img>
+              </v-col>
+              <v-col class="card">
+                <v-img src="/card-back.png"></v-img>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="4">
+            <v-row justify="center">
+              <v-col v-if="showNames" cols="12">
+                <p class="text-center">COMPUTER</p>
+              </v-col>
+              <v-col cols="6" class="card">
+                <v-img src="/card-back.png"></v-img>
+              </v-col>
+              <v-col cols="6" class="card">
+                <v-img src="/card-back.png"></v-img>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="4">
+            <v-row justify="center">
+              <v-col v-if="showNames" cols="12">
+                <p class="text-center">COMPUTER</p>
+              </v-col>
+              <v-col class="card">
+                <v-img src="/card-back.png"></v-img>
+              </v-col>
+              <v-col class="card">
+                <v-img src="/card-back.png"></v-img>
+              </v-col>
+            </v-row>
+          </v-col>
+
+        </v-row>
+
+        <v-row justify="center" align="center">
+          <v-col class="card">
+            <v-img src="/10c.png"></v-img>
+          </v-col>
+          <v-col class="card">
+            <v-img src="/10c.png"></v-img>
+          </v-col>
+          <v-col class="card">
+            <v-img src="/card-back.png"></v-img>
+          </v-col>
+          <v-col class="card">
+            <v-img src="/card-back.png"></v-img>
+          </v-col>
+          <v-col class="card">
+            <v-img src="/card-back.png"></v-img>
+          </v-col>
+        </v-row>
+        
+        <v-row style="transform: translateY(100px)">
+          <v-col cols="4">
+            <v-row justify="center">
+              <v-col v-if="showNames" cols="12">
+                <p class="text-center">COMPUTER</p>
+              </v-col>
+              <v-col class="card">
+                <v-img src="/card-back.png"></v-img>
+              </v-col>
+              <v-col class="card">
+                <v-img src="/card-back.png"></v-img>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="4">
+            <v-row justify="center">
+              <v-col v-if="showNames" cols="12">
+                <p class="text-center">YOU</p>
+              </v-col>
+              <v-col cols="6" class="card">
+                <v-img src="/10c.png"></v-img>
+              </v-col>
+              <v-col cols="6" class="card">
+                <v-img src="/10c.png"></v-img>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="4">
+            <v-row justify="center">
+              <v-col v-if="showNames" cols="12">
+                <p class="text-center">COMPUTER</p>
+              </v-col>
+              <v-col class="card">
+                <v-img src="/card-back.png"></v-img>
+              </v-col>
+              <v-col class="card">
+                <v-img src="/card-back.png"></v-img>
+              </v-col>
+            </v-row>
+          </v-col>
+
+        </v-row>
+
+        
+      </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            showNames: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+  .card {
+    min-width: 100px;
+    max-width: 100px;
+    box-shadow: 0px 0px 19px 0px rgba(0,0,0,0.75);
+    padding: 0;
+    margin: 0 5px 0 5px;
+    
+  }
+  .card > v-img {
+  }
+
+  p {
+      font-weight: 900;
+  }
+
+  .table {
+    background: rgb(0,154,0);
+    background: radial-gradient(circle, rgba(0,154,0,1) 0%, rgba(0,79,16,1) 100%);
+  }
+
+</style>
--- pages/practice.vue
+++ pages/practice.vue
@@ -1,131 +1,7 @@
 <template>
-  <v-container class="py-15"  fill-height>
-    <v-row justify="center">
-       <h1>YOUR TURN</h1>
-    </v-row>
+  <v-container class="py-0"  fill-height>
     <v-row class="mx-0" justify="center">
-
-      <!-- <v-col v-for="n in 50" :key="n" style="min-width: 100px; max-width: 100px;">
-          <v-img src="/10c.png"></v-img>
-        </v-col> -->
-      <!-- <v-row justify="center">
-            <v-row style="position: absolute; bottom: 0"  >
-                <v-col class="card">
-                    <v-img src="/10c.png"></v-img>
-                </v-col>
-                <v-col class="card">
-                    <v-img src="/10c.png"></v-img>
-                </v-col>
-            </v-row>
-        </v-row> -->
-       
-      <v-container style="max-width: 1200px; height: 500px;border-radius: 5%;"  class="table" fill-height>
-
-        <v-row style="transform: translateY(-100px)">
-          <v-col cols="4">
-            <v-row justify="center">
-
-              <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
-              </v-col>
-              <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
-              </v-col>
-              <v-col cols="12">
-                <p class="text-center">COMPUTER</p>
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col cols="4">
-            <v-row justify="center">
-              <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
-              </v-col>
-              <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
-              </v-col>
-              <v-col cols="12">
-                <p class="text-center">COMPUTER</p>
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col cols="4">
-            <v-row justify="center">
-              <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
-              </v-col>
-              <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
-              </v-col>
-              <v-col cols="12">
-                <p class="text-center">COMPUTER</p>
-              </v-col>
-            </v-row>
-          </v-col>
-
-        </v-row>
-        <v-row justify="center">
-          <v-col class="card">
-            <v-img src="/10c.png"></v-img>
-          </v-col>
-          <v-col class="card">
-            <v-img src="/10c.png"></v-img>
-          </v-col>
-          <v-col class="card">
-            <v-img src="/card-back.png"></v-img>
-          </v-col>
-          <v-col class="card">
-            <v-img src="/card-back.png"></v-img>
-          </v-col>
-          <v-col class="card">
-            <v-img src="/card-back.png"></v-img>
-          </v-col>
-        </v-row>
-        <v-row style="transform: translateY(0px)">
-          <v-col cols="4">
-            <v-row justify="center">
-              <v-col cols="12">
-                <p class="text-center">COMPUTER</p>
-              </v-col>
-              <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
-              </v-col>
-              <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col cols="4">
-            <v-row justify="center">
-              <v-col cols="12">
-                <p class="text-center">YOU</p>
-              </v-col>
-              <v-col cols="6" class="card">
-                <v-img src="/10c.png"></v-img>
-              </v-col>
-              <v-col cols="6" class="card">
-                <v-img src="/10c.png"></v-img>
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col cols="4">
-            <v-row justify="center">
-              <v-col cols="12">
-                <p class="text-center">COMPUTER</p>
-              </v-col>
-              <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
-              </v-col>
-              <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
-              </v-col>
-            </v-row>
-          </v-col>
-
-        </v-row>
-
-        
-      </v-container>
+      <poker-table/>
 
     </v-row>
         <!-- <v-row style="width: 100%; margin-top: 100px" class="mx-0">
@@ -148,31 +24,15 @@
 </template>
 
 <script>
-  export default {
+import PokerTable from "@/components/PokerTable.vue";
 
+  export default {
+    components: {PokerTable}
   }
 
 </script>
 
 <style>
-  .card {
-    min-width: 100px;
-    max-width: 100px;
-    box-shadow: 0px 0px 19px 0px rgba(0,0,0,0.75);
-    padding: 0;
-    margin: 0 10px 0 0;
-    
-  }
-  .card > v-img {
-  }
 
-  p {
-      font-weight: 900;
-  }
-
-  .table {
-    background: rgb(0,154,0);
-    background: radial-gradient(circle, rgba(0,154,0,1) 0%, rgba(0,79,16,1) 100%);
-  }
 
 </style>
