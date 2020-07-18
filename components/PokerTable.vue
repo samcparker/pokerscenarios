90629a0fb9dde4d0c0b37ce8cb3a382d26f3c3fe
Sam <samcparker1999@gmail.com>
2020-07-18 10:36:09 +0100

    Added cards and done more on table

--- components/PokerTable.vue
+++ components/PokerTable.vue
@@ -1,10 +1,14 @@
 <template>
 <div>
     <!-- <v-row justify="center" style="margin-bottom: 100px"><h1>Your Turn</h1></v-row> -->
+          <v-btn @click="riverI += 1" style="margin-bottom: 100px">
+        Increase River
+      </v-btn>
   <v-container style="max-width: 1200px; height: 500px; border-radius: 5%;"  class="table" fill-height>
 
     <!-- <v-row style="transform: translateY(-100px)"> -->
     <!-- <v-row style="margin-top: -100px"> -->
+
       <v-row style="margin-top: -100px">
           <v-col cols="4">
             <v-row justify="center">
@@ -12,10 +16,10 @@
                 <p class="text-center">COMPUTER</p>
               </v-col>
               <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
+                <v-img src="/cards/red_back.png"></v-img>
               </v-col>
               <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
+                <v-img src="/cards/red_back.png"></v-img>
               </v-col>
             </v-row>
           </v-col>
@@ -25,10 +29,10 @@
                 <p class="text-center">COMPUTER</p>
               </v-col>
               <v-col cols="6" class="card">
-                <v-img src="/card-back.png"></v-img>
+                <v-img src="/cards/red_back.png"></v-img>
               </v-col>
               <v-col cols="6" class="card">
-                <v-img src="/card-back.png"></v-img>
+                <v-img src="/cards/red_back.png"></v-img>
               </v-col>
             </v-row>
           </v-col>
@@ -38,10 +42,10 @@
                 <p class="text-center">COMPUTER</p>
               </v-col>
               <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
+                <v-img src="/cards/red_back.png"></v-img>
               </v-col>
               <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
+                <v-img src="/cards/red_back.png"></v-img>
               </v-col>
             </v-row>
           </v-col>
@@ -49,20 +53,9 @@
         </v-row>
 
         <v-row justify="center" align="center">
-          <v-col class="card">
-            <v-img src="/10c.png"></v-img>
-          </v-col>
-          <v-col class="card">
-            <v-img src="/10c.png"></v-img>
-          </v-col>
-          <v-col class="card">
-            <v-img src="/card-back.png"></v-img>
-          </v-col>
-          <v-col class="card">
-            <v-img src="/card-back.png"></v-img>
-          </v-col>
-          <v-col class="card">
-            <v-img src="/card-back.png"></v-img>
+          <v-col v-for="n in 5" :key="n" class="card">
+            <v-img v-if="riverI >= n" :src="'/cards/' + river[n - 1] + '.png'"></v-img>
+            <v-img v-else src="/cards/red_back.png"></v-img>
           </v-col>
         </v-row>
         
@@ -74,10 +67,10 @@
                 <p class="text-center">COMPUTER</p>
               </v-col>
               <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
+                <v-img src="/cards/red_back.png"></v-img>
               </v-col>
               <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
+                <v-img src="/cards/red_back.png"></v-img>
               </v-col>
             </v-row>
           </v-col>
@@ -87,10 +80,12 @@
                 <p class="text-center">YOU</p>
               </v-col>
               <v-col cols="6" class="card">
-                <v-img src="/10c.png"></v-img>
+                <v-img v-if="playersCards.length != 0" :src="'/cards/' + playersCards[0][0] + '.png'"></v-img>
+                <v-img v-else :src="'/cards/red_back.png'"></v-img>
               </v-col>
               <v-col cols="6" class="card">
-                <v-img src="/10c.png"></v-img>
+                <v-img v-if="playersCards.length != 0" :src="'/cards/' + playersCards[0][1] + '.png'"></v-img>
+                <v-img v-else :src="'/cards/red_back.png'"></v-img>
               </v-col>
             </v-row>
           </v-col>
@@ -100,10 +95,10 @@
                 <p class="text-center">COMPUTER</p>
               </v-col>
               <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
+                <v-img src="/cards/red_back.png"></v-img>
               </v-col>
               <v-col class="card">
-                <v-img src="/card-back.png"></v-img>
+                <v-img src="/cards/red_back.png"></v-img>
               </v-col>
             </v-row>
           </v-col>
@@ -119,8 +114,62 @@
 export default {
     data: () => {
         return {
-            showNames: false
+          cards: [],
+          showNames: false,
+          playersCards: [], // going clockwise starting at the player's position
+          river: [],
+          riverI: 2
         }
+    },
+    computed: {
+
+    },
+    methods: {
+      sourceFromCard(card) {
+        return "/cards/" + card + ".png";
+      },
+      resetCards() {
+        const suits = ["C", "D", "H", "S"];
+        const pictureCards = ["A", "J", "Q", "K"];
+        var cards = [];
+
+        for (var i in suits) {
+          for (var j = 2; j <= 10; j++) {
+            cards.push(j + suits[i]);
+          }
+          for (var j in pictureCards) {
+            cards.push(pictureCards[j] + suits[i]);
+          }
+        }
+
+        this.cards = cards;
+      },
+      getRandomCards(k) {
+        var gen = [];
+        for (var i = 0; i < k; i++) {
+          gen.push(this.getRandom());
+        }
+        return gen;
+      },
+      getRandom() {
+        var i = Math.floor((Math.random() * this.cards.length));
+        var card = this.cards[i];
+        this.cards = this.cards.filter(e => { return e != this.cards[i] });
+
+        return card;        
+      }
+    },
+    mounted () {
+      this.resetCards();
+      
+      this.playerCards = this.getRandomCards(2);
+
+      for (var i = 0; i < 6; i++) {
+        this.playersCards.push(this.getRandomCards(2));
+      }
+
+      this.river = this.getRandomCards(5);
+      console.log(this.river);
     }
 }
 </script>
